081b00184190930818cfdcc893edb76779ed67e8
MCTOnlinewebbot <marioanatoly1987@gmail.com>
2026-04-02 16:33:32 +0300

    Rename run function to run_server for clarity
    Fixing keep-alive to allow bot logic to run in background.

--- keep_alive.py
+++ keep_alive.py
@@ -7,10 +7,9 @@ app = Flask('')
 def home():
     return "MCT Bot is Alive!"
 
-def run():
+def run_server(): # Renamed to avoid confusion
     app.run(host='0.0.0.0', port=8080)
 
-def keep_alive():
-    t = Thread(target=run)
+def keep_alive(): # This is the function that starts the thread
+    t = Thread(target=run_server)
     t.start()
-
